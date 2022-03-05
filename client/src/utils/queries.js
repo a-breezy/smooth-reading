14079b50cc08e9b360ca1849b77d8176ddecfdce
a.breezy <ambrose.wilkins@gmail.com>
2022-03-05 12:08:47 -0500

    update SavedBooks and SearchBooks.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,13 @@
+import {gql} from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks
+
+        // add the rest of the mutation
+    }
+`;
